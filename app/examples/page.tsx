72c30d43509e0fca12f942216ad0004fa3649c21
Mckay Wrigley <mckaywrigley@gmail.com>
2024-05-13 02:52:20 -0600

    add me section

---
+++ app/examples/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <div>example code</div>
+    </div>
+  );
+}
